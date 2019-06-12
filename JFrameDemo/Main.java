c08074e8373a2090f5a267ebbb863ab16b6fa4f5
DavidHsin <daxian2392@gmail.com>
2019-06-12 10:10:58 +0800

    Update Main.java

--- JFrameDemo/Main.java
+++ JFrameDemo/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//把GUI创建的代码放进新的线程（事件分发线程）中
 		EventQueue.invokeLater(() -> {
-			AlgoFrame frame = new AlgoFrame("welcome");
+			AlgoFrame frame = new AlgoFrame("welcome", 500, 500);
 			//AlgoFrame frame = new AlgoFrame("Welcome", 500, 500);
 		});
 		
